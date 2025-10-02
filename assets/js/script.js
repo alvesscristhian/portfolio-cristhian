bb33e713d6c0f0a6efcab31170adeb9ce90ad10e
Cristhian Alves <cristhian.ads@hotmail.com>
2025-10-02 01:54:36 -0300

    :sparkles: Interatividade com JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+window.addEventListener("scroll", () => {
+  document.querySelectorAll(".reveal").forEach(el => {
+    const rect = el.getBoundingClientRect();
+    if (rect.top < window.innerHeight - 100) {
+      el.classList.add("active");
+    }
+  });
+});
+
+const toggleButton = document.getElementById("toggle-theme");
+toggleButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        toggleButton.textContent = "☀️"; 
+    } else {
+        toggleButton.textContent = "🌙"; 
+    }
+});
