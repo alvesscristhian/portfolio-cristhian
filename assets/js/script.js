958e449e67cece27cbbe623717afe315f3d29946
Cristhian Alves <cristhian.ads@hotmail.com>
2025-10-04 03:08:21 -0300

    :rocket: fix: Problemas na seção Projetos

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,28 @@ window.addEventListener("scroll", () => {
 
 const toggleButton = document.getElementById("toggle-theme");
 toggleButton.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
+  document.body.classList.toggle("dark-mode");
 
-    if (document.body.classList.contains("dark-mode")) {
-        toggleButton.textContent = "☀️"; 
-    } else {
-        toggleButton.textContent = "🌙"; 
+  if (document.body.classList.contains("dark-mode")) {
+    toggleButton.textContent = "☀️";
+  } else {
+    toggleButton.textContent = "🌙";
+  }
+});
+
+const cards = document.querySelectorAll(".cards");
+cards.forEach(card => {
+  card.addEventListener('mouseenter', e => {
+    switch (e.target.id) {
+      case "landing-salao":
+        document.querySelector('#link-salao').style.opacity = 1;
+        break;
+      case "cadastro-alunos":
+        document.querySelector('#link-avaliacao').style.opacity = 1;
+        break;
+      case "valida-form":
+        document.querySelector('#link-valida').style.opacity = 1;
+        break;
     }
+  });
 });
