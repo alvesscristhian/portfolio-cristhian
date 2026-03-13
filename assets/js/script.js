c42f8a92a3f0c51a796cfd6718b3b4d0f264ae7f
Cristhian Alves <cristhian.ads@hotmail.com>
2026-03-13 13:22:47 -0300

    style: Estilização melhor no site

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,32 +7,11 @@ window.addEventListener("scroll", () => {
   });
 });
 
+// Alternância de Tema Escuro/Claro
 const toggleButton = document.getElementById("toggle-theme");
-toggleButton.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
-});
-
-const cards = document.querySelectorAll(".cards");
-if (window.matchMedia("(width > 1024px)")) {
-  cards.forEach(card => {
-    card.addEventListener('mouseenter', e => {
-      switch (e.target.id) {
-        case "landing-salao":
-          document.querySelector('#link-salao').style.opacity = 1;
-          document.querySelector('#link-avaliacao').style.opacity = 0;
-          document.querySelector('#link-valida').style.opacity = 0;
-          break;
-        case "cadastro-alunos":
-          document.querySelector('#link-avaliacao').style.opacity = 1;
-          document.querySelector('#link-salao').style.opacity = 0;
-          document.querySelector('#link-valida').style.opacity = 0;
-          break;
-        case "valida-form":
-          document.querySelector('#link-valida').style.opacity = 1;
-          document.querySelector('#link-avaliacao').style.opacity = 0;
-          document.querySelector('#link-salao').style.opacity = 0;
-          break;
-      }
-    });
+if (toggleButton) {
+  toggleButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    document.querySelector("header.menu").classList.toggle("dark-mode");
   });
 }
